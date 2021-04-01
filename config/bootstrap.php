15b0fd02f46108cb363679912e6c69ef699377cd
peteratnsight <pp@n-sight.de>
2021-04-01 11:09:06 +0200

    1.0

---
+++ config/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+use Cake\Core\Configure;
+use Cake\Core\Exception\MissingPluginException;
+use Cake\Core\Plugin;
+
+//if (!Configure::check('BackendTheme.prefix')) {
+//    Configure::write('BackendTheme.prefix', 'default-');
+//}
